5badb5d3d8d541909cab693fcd2b400d4127e40f
benchvue <benchvue101@gmail.com>
2025-11-08 22:10:55 -0500

    Add LED color control functionality

---
+++ set-color.ino
@@ -0,0 +1,39 @@
+#include <Modulino.h>
+
+// Create a ModulinoPixels object
+ModulinoPixels leds;
+
+int brightness = 10;
+
+// Function to set all LEDs to the same color at once
+void set_color(uint8_t color) {
+  for (int i = 0; i < 8; i++) {
+    leds.set(i, color, brightness);
+  }
+  leds.show();
+}
+
+void setup(){
+  // Initialize Modulino I2C communication
+  Modulino.begin();
+  // Detect and connect to pixels module
+  leds.begin();
+}
+
+void loop(){
+  // Now you can easily change colors
+  set_color(GREEN);
+  delay(1000);
+  
+  set_color(BLUE);
+  delay(1000);
+  
+  set_color(RED);
+  delay(1000);
+  
+  set_color(VIOLET);
+  delay(1000);
+  
+  set_color(WHITE);
+  delay(1000);
+}
